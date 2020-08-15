42b999f8b578f899c651caff6988f169bc3307e5
David <davidcharlesv@gmail.com>
2020-08-15 16:58:10 -0700

    updates

--- Components/getFriendList/getFriends/ViewController.swift
+++ Components/getFriendList/getFriends/ViewController.swift
@@ -14,12 +14,26 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getUserFriends(userName: "Vasquezd") { tempFriendsArray, error in
+            DispatchQueue.main.async {
+                self.friendsArray = tempFriendsArray
+                
+            }
+        }
     }
     
-    //Actions
+    //Get All Posts
     @IBAction func getFriends(_ sender: Any) {
-        getFriends()
+        getUserFriends(userName: "Vasquezd") { tempFriendsArray, error in
+            DispatchQueue.main.async {
+                self.friendsArray = tempFriendsArray
+                
+            }
+        }
+        //var friendsTempArray = getUserFriends(userName: "Vasquezd")
+        //print("My Friends: \(friendsTempArray)")
     }
+    
     
     @IBAction func showFriends(_ sender: Any) {
         for friend in friendsArray {
@@ -29,37 +43,6 @@ class ViewController: UIViewController {
     }
     
     
-    func getFriends() {
-        //Get Friends
-        let parameters = ["user_name": "vasquezd", "user_key": "vasquezd"]
-        
-        guard let url = URL(string: "http://people.oregonstate.edu/~vasquezd/sites/template/site_files/rest/friends/get_friends.php?user_name=vasquezd") else { return }
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return }
-        request.httpBody = httpBody
-        
-        let session = URLSession.shared
-        session.dataTask(with: request) { (data, response, error) in
-            if let data = data {
-                do {
-                    self.friendsArray.removeAll()
-                    let friendModelArray = try JSONDecoder().decode([FriendModel].self, from: data)
-                    
-                    for friend in friendModelArray{
-                        let currentFriend = Friend(userName: friend.friend_user_name)
-                        currentFriend.userImage = friend.user_image
-                        currentFriend.firstName = friend.first_name
-                        currentFriend.lastName = friend.last_name
-                        self.friendsArray.append(currentFriend)
-                    }
-                } catch {
-                    print(error)
-                }
-            }
-            
-            }.resume()
-    }
+
 }
 
