ccf44f0e40b8beb5243965194e16bd05501b7880
David <vasquezd@oregonstate.edu>
2020-12-12 17:43:56 -0800

    updates

---
+++ Post/Post/Post/PostsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PostsViewController.swift
+//  Post
+//
+//  Created by David Vasquez on 12/12/20.
+//  Copyright © 2020 David Vasquez. All rights reserved.
+//
+
+import UIKit
+
+class PostsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
---
+++ Post/Post/Post/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Post
+//
+//  Created by David Vasquez on 12/12/20.
+//  Copyright © 2020 David Vasquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
